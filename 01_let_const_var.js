3e9b0d868a2e1a1a164333b51e5497327903e775
PrikshitBuilds <prikshitbuilds@gmail.com>
2025-06-22 23:32:24 +0530

    Add files via upload

---
+++ 01_let_const_var.js
@@ -0,0 +1,17 @@
+console.log("Prikshit");
+
+const accountId = 1234
+let accountEmail ="Prikshit@gmail.com"
+var accountPassword = "12345"
+accountCity ="Jaipur"
+
+
+accountEmail="hc@gmail.com"
+accountPassword="12321"
+accountCity="delhi"
+
+
+console.table([accountEmail,accountPassword,accountCity])
+
+// Prefer not to use var -> block scope and functional scope
+//  
