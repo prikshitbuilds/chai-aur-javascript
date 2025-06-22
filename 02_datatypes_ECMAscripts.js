e947e2566080a882ad29b9c51827e8b4cafd1ea6
PrikshitBuilds <prikshitbuilds@gmail.com>
2025-06-22 23:53:27 +0530

    Add files via upload

---
+++ 02_datatypes_ECMAscripts.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+
+console.log(3+3);
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+//Data-Types
+
+//Number
+//Big-Int
+//String =>""
+//Boolean => false true
+//Null => stanalone value
+//Undefined => 
+// Symbol =>  unique
+
+
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
---
+++ 03_Conversion.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 33abc => NaN
+// null => 0
+// undefined => NaN
+// true -> 1
+// false-> 0
+
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1 -> true
+// 0 , "" -> false
+// "hitesh" -> true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
