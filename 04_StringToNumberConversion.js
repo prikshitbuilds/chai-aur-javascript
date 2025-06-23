cda4bf4db149c3b9bad7fe6410b05916e21896f3
PrikshitBuilds <prikshitbuilds@gmail.com>
2025-06-23 13:07:07 +0530

    Add files via upload

---
+++ 04_StringToNumberConversion.js
@@ -0,0 +1,34 @@
+//  Operations
+
+let value = 9
+let negValue = -value
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+
+
+let str1="Hello"
+let str2=" Prikshit"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2); //12 
+console.log(1+ "2");//12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+console.log(+true); //1
+console.log(+"");//0
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
---
+++ 05_Comparison.js
@@ -0,0 +1,28 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+
+
+
+
+console.log("2">1); // true
+console.log("02">1); // true
+
+
+
+console.log(null > 0); // false
+console.log(null ==0); // false
+console.log(null >=0); // true
+// comparison converts null to number treating it as 0 .
+
+
+
+console.log(undefined === 0); // false
+console.log(undefined>0); // false 
+console.log(undefined<0);// false
+
+
+// === => strict check
+
+console.log("2"===2); // false
