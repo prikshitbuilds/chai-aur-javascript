4bc7c4bd2c1657e576d565abedfe2be89ca3d4ca
PrikshitBuilds <prikshitbuilds@gmail.com>
2025-07-10 17:36:30 +0530

    Add files via upload

---
+++ 21_IIFE.js
@@ -0,0 +1,20 @@
+
+(function chai(){
+    //Named iffie
+    console.log(`DB CONNCETED`);
+})();
+
+// ; context rokne ke liye
+
+
+
+// (fnc defintion)(execution call)
+
+(()=>{
+console.log(`DB CONNECTED TWO`);
+})();
+
+
+((name)=>{
+console.log(`DB CONNECTED ${name}`);
+})('prikshit')
