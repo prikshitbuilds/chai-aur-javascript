c623013a23f195476ff4ffcbe5bdac1584a9ad5e
PrikshitBuilds <prikshitbuilds@gmail.com>
2025-07-10 16:30:59 +0530

    Add files via upload

---
+++ 19_ScopeLevel_MiniHoisting.js
@@ -0,0 +1,44 @@
+
+function one(){
+ const username ="Prikshit"
+
+ function two(){
+    const website = "youtube"
+    console.log(username);
+ }
+// console.log(website);
+ two();
+}
+
+one();
+
+
+if(true){
+    const username ="prikshit"
+    if(username=== "prikshit"){
+        const website = "  youtube"
+        console.log(username +website);
+    }
+}
+
+function addOne(num){
+    return num +1
+}
+
+
+
+const addTwo = function(num){
+    return num +2
+}
+
+
+
+
+
+
+
+
+
+
+
+
